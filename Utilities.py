e9730117e750f088e3c0d52d22baa254016a43a1
Aravind Karnam R <aravind.karanam@gmail.com>
2019-10-07 22:00:54 +0530

    made some minor changes to table names

--- Utilities.py
+++ Utilities.py
@@ -1,3 +1,18 @@
+import time
+
+
+def measure_time(func):
+
+    def measurer(*args, **kwargs):
+        begin = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        print("Total time taken in : ", func.__name__, end - begin)
+
+    return measurer
+
+
+
 import glob
 import os
 import pandas as pd
